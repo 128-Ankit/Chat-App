fcbba89103fe9bf1746fa5751286830e52b81f55
Ankit Kumar <abhishekankit128@gmail.com>
2024-05-19 00:41:52 +0530

    Created user schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["male", "female"],
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+},
+    { timestamps: true });
+
+export default mongoose.model("User", userSchema);
