224ec54fd7731b39254460322ca782d59818b356
Ankit Kumar <abhishekankit128@gmail.com>
2024-05-19 10:48:19 +0530

    created message route

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,20 @@
+const Message = require("../models/messageModel");
+
+// Sending message
+const sendMessage = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+// Receiving message
+const getMessage = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+module.exports = { sendMessage, getMessage };
--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
 // Require controller modules.
+const { sendMessage, getMessage } = require("../controllers/messageController");
 
-router.get('/msgRouter', (req,res) => {
-    res.json({msg: "Welcome to the MessageRouter"});
-});
+router.post('/send/:id', sendMessage);
+router.post('/:id', getMessage);
 
-module.exports = router
+module.exports = router;
