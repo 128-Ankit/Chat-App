d782d9c545c93365607a70f4519c9752f113e1c3
Ankit Kumar <abhishekankit128@gmail.com>
2024-05-20 12:04:09 +0530

    done with login and logout (backend + frontend)

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../constext/AuthContext";
 
 const useSignup = () => {
     const [loading, setLoading] = useState(false);
+    const { setAuthUser } = useAuthContext();
 
     const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
         const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
@@ -23,6 +25,9 @@ const useSignup = () => {
             else if (data.error) {
                 throw new Error(data.error);
             }
+            //saving user in local storage
+            localStorage.setItem("chat-user", JSON.stringify(data));
+			setAuthUser(data);
         } catch (error) {
             toast.error(error.message);
         } finally {
